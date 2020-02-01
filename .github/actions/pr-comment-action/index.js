ebd58437a06c5d5509101a47852f532db0432fdf
Jim Lawson <ucbjrl@berkeley.edu>
2020-01-31 16:27:12 -0800

    Change event type to pull_request; tighten up event checks.

--- .github/actions/pr-comment-action/index.js
+++ .github/actions/pr-comment-action/index.js
@@ -1,20 +1,20 @@
 const core = require('@actions/core');
-const { context, GitHub } = require("@actions/github");
+const context = require("@actions/github");
 
 async function run() {
   if (
-      context.eventName === "issue_comment" &&
-      !context.payload.issue.pull_request
+      context.eventName !== "pull_request" ||
+      !context.payload.pull_request
   ) {
       // not a pull-request comment, aborting
-      core.setOutput("comment", "error: not a pull-request comment");
+      core.setOutput("comment", "error: not a pull-request");
       return;
   }
 
   const { owner, repo } = context.repo;
 
   const body =
-      context.eventName === "issue_comment"
+      context.eventName !== "pull_request"
           ? context.payload.comment.body
           : context.payload.pull_request.body;
 
