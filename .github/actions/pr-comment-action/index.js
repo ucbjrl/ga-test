0627bdb73bbfbf6db05287ce64aaf41ce65c0753
Jim Lawson <ucbjrl@berkeley.edu>
2020-02-07 16:07:10 -0800

    Accept any event.

--- .github/actions/pr-comment-action/index.js
+++ .github/actions/pr-comment-action/index.js
@@ -2,21 +2,11 @@ const core = require('@actions/core');
 const context = require("@actions/github");
 
 async function run() {
-  if (
-      context.eventName !== "pull_request" ||
-      !context.payload.pull_request
-  ) {
-      // not a pull-request comment, aborting
-      core.setOutput("comment", "error: not a pull-request");
-      return;
-  }
+  const name = context.eventName;
 
-  const { owner, repo } = context.repo;
+  const body = context.event.head_commit.message;
 
-  const body =
-      context.event.head_commit.message;
-
-  core.setOutput("comment", body);
+  core.setOutput("comment", name + "\n" + body);
 
 }
 
