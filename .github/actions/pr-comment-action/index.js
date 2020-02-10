05ae6317f13cf3d57d7a36752abd5f952a3ec1dd
Jim Lawson <ucbjrl@berkeley.edu>
2020-02-10 08:37:57 -0800

    Try to fetch payload; wrap output in quotes

--- .github/actions/pr-comment-action/index.js
+++ .github/actions/pr-comment-action/index.js
@@ -5,7 +5,7 @@ const context = github.context;
 async function run() {
   const name = github.context.eventName;
 
-  const body = "(no body)"; //context.payload.head_commit.message;
+  const body = /* "(no body)"; */ github.payload.head_commit.message;
 
   core.setOutput("comment", name + "\n" + body);
 
